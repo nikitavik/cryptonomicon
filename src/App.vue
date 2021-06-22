fa140d77ad42c38ce9e2131fb036fcbda0c7c3b7
Nikita Kornilov <nikitakornilov1998@gmail.com>
2021-06-22 15:20:59 +0300

    Fix graph

--- src/App.vue
+++ src/App.vue
@@ -249,7 +249,15 @@ export default {
   },
   methods: {
     updateTicker(tickerName, price) {
-      this.tickers.filter(t => t.name === tickerName).forEach(t => {t.price = price})
+      this.tickers.
+      filter(t => t.name === tickerName)
+          .forEach(t => {
+            if (t === this.selectedTicker){
+              this.graph.push(price)
+            }
+            t.price = price
+          })
+
     },
     // Price formatting
     formatPrice(price) {
