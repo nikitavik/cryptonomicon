ec234ea71cffdb100b131f2e3ebf867f22d69138
Nikita Kornilov <nikitakornilov1998@gmail.com>
2021-07-01 07:19:57 +0300

    Move loading spinner to separate component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
-<!--    Spinner-->
-    <div
-        v-if="!loaded"
-        class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">
-      <svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-      </svg>
-    </div>
-<!--    Main-->
+    <loading-spinner
+        :loaded="loaded"
+    />
     <div class="container">
       <add-ticker
           @add-ticker="add"
@@ -101,10 +94,12 @@
 import { subscribeToTicker, unsubscribeFromTicker } from "@/api";
 import AddTicker from "@/components/AddTicker";
 import TickerGraph from "@/components/TickerGraph";
+import LoadingSpinner from "@/components/LoadingSpinner";
 
 export default {
   name: 'App',
   components: {
+    LoadingSpinner,
     TickerGraph,
     AddTicker,
   },
