3054717c03a963d788e2bd33d72062cf78e7bf0d
Nikita Kornilov <nikitakornilov1998@gmail.com>
2021-07-01 06:55:43 +0300

    Migrate to Vue3

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
-import Vue from 'vue'
+import {createApp} from "vue"
 import App from './App.vue'
 import './assets/tailwind.css'
 
-Vue.config.productionTip = false
-Vue.config.devtools = true
+// Vue.config.devtools = true
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+createApp(App).mount("#app")
